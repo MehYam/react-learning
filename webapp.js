5ae5967805d99306487c29549db2441eb6920a3f
Kai Arnold <cdn.kai@gmail.com>
2017-07-27 14:05:27 -0700

    step 5.1

--- webapp.js
+++ webapp.js
@@ -7,6 +7,14 @@ const app = express()
 
 app.use(express.static('static'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get('/api/users', (req, res) => res.status(200).send(JSON.stringify(users[requests++ % users.length])) );
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+var requests = 0;
+var users = 
+[
+    {id:"1", firstName: "Kai", lastName: "Arnold"},
+    {id:"2", firstName: "Kira", lastName: "Tsu"},
+    {id:"3", firstName: "Piper", lastName: "BoBo"}
+];
