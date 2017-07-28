6b0d4e8c5cde085ec2a9b6da877a8a733ca1a5e1
Kai Arnold <cdn.kai@gmail.com>
2017-07-28 14:58:47 -0700

    remove local user array

--- webapp.js
+++ webapp.js
@@ -3,26 +3,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-class Users {
-   constructor() {
-      this.users = [];
-      this.currentID = 0;
-   }
-   add(user) {
-      user.id = this.currentID++;
-      this.users.push(user);
-   }
-   get list() { return this.users; }
-   get JSON() { return JSON.stringify(this.users); }
-}
-
-const users = new Users();
-users.add({firstName: "Kai", lastName: "Arnold"});
-users.add({firstName: "Kira", lastName: "Tsu"});
-users.add({firstName: "Piper", lastName: "BoBo"});
-
-////////////////////////////////////////////
-
 // app.get('/', function (req, res) {
 //   res.send('Hello World - from webapp.js')
 // })
@@ -43,8 +23,6 @@ app.post('/api/users/add', (req, res) =>
    console.log("request body: ", req.body);
 
    let user = req.body;
-   // users.add(user);
-   // res.json(user);
 
    database.collection(table).insertOne(user, (err, result) => {
 
